61352d1e5672ca87940792cf4343557e54409ec1
NataliaJeszke <nataliajeszke8@gmail.com>
2022-12-14 22:23:55 +0100

    first try on changing styles with dark mode option

--- js.js
+++ js.js
@@ -1,4 +1,39 @@
-function darkMode(){
-    const moon = document.body;
-    moon.classList.toggle("dark-mode");
-}
+// function darkMode(){
+//     const moon = document.body;
+//     moon.classList.toggle("dark-mode");
+// }
+
+const bodyBg = "#000000";
+const fontColorDark="#EEEEEE";
+const backgroundDivs="#424242";
+const socialBtn="#BDBDBD";
+
+const moonBtn = document.querySelector(".fa-moon");
+
+moonBtn.addEventListener("click", ()=>{
+document.querySelector("body").style.backgroundColor=bodyBg;
+//header style
+document.querySelector(".header").style.backgroundColor=backgroundDivs;
+document.querySelector(".navContainer").style.backgroundColor=backgroundDivs;
+document.querySelector(".nav-link1").style.color=fontColorDark;
+document.querySelector(".nav-link2").style.color=fontColorDark;
+document.querySelector(".nav-link3").style.color=fontColorDark;
+document.querySelector(".nav-link4").style.color=fontColorDark;
+//main section
+document.querySelector(".mainDiv").style.backgroundColor=backgroundDivs;
+document.querySelector(".textContainer").style.backgroundColor=backgroundDivs;
+document.querySelector(".socialMediaContainer").style.backgroundColor=backgroundDivs;
+document.querySelector(".contactBtn").style.backgroundColor=bodyBg;
+////text style in main section
+document.querySelector("h1").style.color=fontColorDark;
+document.querySelector("p").style.color=fontColorDark;
+document.querySelector("h2").style.color=fontColorDark;
+document.querySelector(".anotherH2").style.color=fontColorDark;
+document.querySelector("h3").style.color=fontColorDark;
+document.querySelector("button").style.color=fontColorDark;
+////img style
+document.querySelector("img").style.opacity=0.8;
+//second section
+document.querySelector(".secondContainer").style.backgroundColor=backgroundDivs;
+
+});
