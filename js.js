7c3ce465e9f0701b07ecb25dd7876ca16bb3c98f
NataliaJeszke <nataliajeszke8@gmail.com>
2022-12-15 20:31:25 +0100

    working version of Dark Mode with change icon from moon to sun

--- js.js
+++ js.js
@@ -1,54 +1,39 @@
-/////First try on dark mode/////
-// function darkMode(){
-//     const moon = document.body;
-//     moon.classList.toggle("dark-mode");
-// }
+/// TOGGLE DARK MODE WITH ICON CHANGE ///
+let darkMode = localStorage.getItem("darkMode");
+console.log(darkMode);
+const darkModeToggle = document.querySelector("#moon");
 
-/////Second try on dark mode/////
-// const bodyBg = "#000000";
-// const fontColorDark="#EEEEEE";
-// const backgroundDivs="#424242";
-// const socialBtn="#BDBDBD";
+const enableDarkMode = () =>{
+    document.body.classList.add("dark");
+    localStorage.setItem("darkMode", "enabled");
+}
+const disDarkMode = () =>{
+    document.body.classList.remove("dark");
+    localStorage.removeItem("darkMode");
+}
 
-/////Third try on dark mode/////
-const moonBtn = document.querySelector(".fa-moon");
-moonBtn.addEventListener("click", ()=>{
-    document.querySelector("body").classList.toggle("dark");
-        document.getElementById("moon").classList.remove("fa-solid");
-        document.getElementById("moon").classList.remove("fa-moon");
-        document.getElementById("moon").classList.remove("fa-2xl");
-        document.getElementById("moon").classList.remove("p-1");
-        document.getElementById("moon").classList.add("fa-solid");
-        document.getElementById("moon").classList.add("fa-sun");
-        document.getElementById("moon").classList.add("fa-2xl");
-        document.getElementById("moon").classList.add("p-1");
-    
+if (darkMode === "enabled"){
+    enableDarkMode();
+}
 
 
+darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    let icon = document.getElementById("moon");
+    if (darkMode !== "enabled"){
+        enableDarkMode();
+        icon.classList.remove("fa-solid");
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-solid")
+        icon.classList.add("fa-sun")
+        
+    } else {
+        disDarkMode();
+        icon.classList.remove("fa-solid");
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-solid")
+        icon.classList.add("fa-moon")
+        
+    }
 
-
-// document.querySelector("body").style.backgroundColor=bodyBg;
-// //header style
-// document.querySelector(".header").style.backgroundColor=backgroundDivs;
-// document.querySelector(".navContainer").style.backgroundColor=backgroundDivs;
-// document.querySelector(".nav-link1").style.color=fontColorDark;
-// document.querySelector(".nav-link2").style.color=fontColorDark;
-// document.querySelector(".nav-link3").style.color=fontColorDark;
-// document.querySelector(".nav-link4").style.color=fontColorDark;
-// //main section
-// document.querySelector(".mainDiv").style.backgroundColor=backgroundDivs;
-// document.querySelector(".textContainer").style.backgroundColor=backgroundDivs;
-// document.querySelector(".socialMediaContainer").style.backgroundColor=backgroundDivs;
-// document.querySelector(".contactBtn").style.backgroundColor=bodyBg;
-// ////text style in main section
-// document.querySelector("h1").style.color=fontColorDark;
-// document.querySelector("p").style.color=fontColorDark;
-// document.querySelector("h2").style.color=fontColorDark;
-// document.querySelector(".anotherH2").style.color=fontColorDark;
-// document.querySelector("h3").style.color=fontColorDark;
-// document.querySelector("button").style.color=fontColorDark;
-// ////img style
-// document.querySelector("img").style.opacity=0.8;
-// //second section
-// document.querySelector(".secondContainer").style.backgroundColor=backgroundDivs;
 });
