478b60b0bfc910ce18c3b8b929ae8ea321ac8d98
NataliaJeszke <nataliajeszke8@gmail.com>
2023-02-19 19:13:13 +0100

    submit form

--- script.js
+++ script.js
@@ -3,51 +3,50 @@ let darkMode = localStorage.getItem("darkMode");
 console.log(darkMode);
 const darkModeToggle = document.querySelector("#moon");
 
-const enableDarkMode = () =>{
-    document.body.classList.add("dark");
-    localStorage.setItem("darkMode", "enabled");
-}
-const disDarkMode = () =>{
-    document.body.classList.remove("dark");
-    localStorage.removeItem("darkMode");
-}
+const enableDarkMode = () => {
+  document.body.classList.add("dark");
+  localStorage.setItem("darkMode", "enabled");
+};
+const disDarkMode = () => {
+  document.body.classList.remove("dark");
+  localStorage.removeItem("darkMode");
+};
 
-if (darkMode === "enabled"){
-    enableDarkMode();
+if (darkMode === "enabled") {
+  enableDarkMode();
 }
-
 
 darkModeToggle.addEventListener("click", () => {
-    darkMode = localStorage.getItem("darkMode");
-    let icon = document.getElementById("moon");
-    if (darkMode !== "enabled"){
-        enableDarkMode();
-        icon.classList.remove("fa-solid");
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-solid")
-        icon.classList.add("fa-sun")
-        
-    } else {
-        disDarkMode();
-        icon.classList.remove("fa-solid");
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-solid")
-        icon.classList.add("fa-moon")
-        
-    }
-
+  darkMode = localStorage.getItem("darkMode");
+  let icon = document.getElementById("moon");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+    icon.classList.remove("fa-solid");
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-sun");
+  } else {
+    disDarkMode();
+    icon.classList.remove("fa-solid");
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-moon");
+  }
 });
 
 ///ONLOAD Function - for checking moon/sun icon
 
-window.onload = function() {
-    let reloading = localStorage.getItem("darkMode");
-    let icon = document.getElementById("moon");
-    if (reloading === "enabled") {
-        console.log(reloading);
-        icon.classList.remove("fa-solid");
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-solid")
-        icon.classList.add("fa-sun")
-    }
-}
+window.onload = function () {
+  let reloading = localStorage.getItem("darkMode");
+  let icon = document.getElementById("moon");
+  if (reloading === "enabled") {
+    console.log(reloading);
+    icon.classList.remove("fa-solid");
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-sun");
+  }
+};
+
+//Send E-mail
+
